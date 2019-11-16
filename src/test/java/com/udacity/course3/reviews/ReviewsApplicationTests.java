9f29fcad5055da75320e5026dfddff689cfb697e
Miguel Flores Silverio <floresmigu3l@gmail.com>
2019-11-15 16:36:08 -0800

    unused import

--- src/test/java/com/udacity/course3/reviews/ReviewsApplicationTests.java
+++ src/test/java/com/udacity/course3/reviews/ReviewsApplicationTests.java
@@ -3,7 +3,6 @@ package com.udacity.course3.reviews;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
