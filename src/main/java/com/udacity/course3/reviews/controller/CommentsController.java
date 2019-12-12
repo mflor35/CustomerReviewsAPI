1968e7696aa9a0cbf42ee55e9033d8f01d010cdb
Miguel Flores Silverio <floresmigu3l@gmail.com>
2019-12-11 23:22:48 -0800

    feat() - retrieve comment from mongodb

--- src/main/java/com/udacity/course3/reviews/controller/CommentsController.java
+++ src/main/java/com/udacity/course3/reviews/controller/CommentsController.java
@@ -5,6 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +13,8 @@ import javax.validation.Valid;
 
 import com.udacity.course3.reviews.entity.Comment;
 import com.udacity.course3.reviews.entity.Review;
+import com.udacity.course3.reviews.entity.MongoComment;
+import com.udacity.course3.reviews.repository.CommentMongoRepository;
 import com.udacity.course3.reviews.repository.CommentRepository;
 import com.udacity.course3.reviews.repository.ReviewRepository;
 
@@ -25,11 +28,13 @@ public class CommentsController {
     // DONE: Wire needed JPA repositories here
     private ReviewRepository reviewRepository;
     private CommentRepository commentRepository;
+    private CommentMongoRepository commentMongoRepo;
 
     @Autowired
-    public CommentsController(ReviewRepository reviewRepository, CommentRepository commentRepository) {
+    public CommentsController(ReviewRepository reviewRepository, CommentRepository commentRepository, CommentMongoRepository commentMongoRepository) {
         this.reviewRepository = reviewRepository;
         this.commentRepository = commentRepository;
+        this.commentMongoRepo = commentMongoRepository;
     }
 
     /**
@@ -43,14 +48,17 @@ public class CommentsController {
      * @param reviewId The id of the review.
      */
     @RequestMapping(value = "/reviews/{reviewId}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Comment> createCommentForReview(@Valid @PathVariable("reviewId") Integer reviewId, @RequestBody Comment comment) {
+    public ResponseEntity<MongoComment> createCommentForReview(@Valid @PathVariable("reviewId") Integer reviewId, @RequestBody Comment comment) {
         Optional<Review> oReview = reviewRepository.findById(reviewId);
         if(!oReview.isPresent()) {
             return ResponseEntity.notFound().build();
         }
         
         comment.setReview(oReview.get());
-        return ResponseEntity.ok(commentRepository.save(comment));
+        comment = commentRepository.save(comment);
+        MongoComment mongoComment = new MongoComment(comment.getTitle(), comment.getBody(), comment.getId());
+
+        return ResponseEntity.ok(commentMongoRepo.save(mongoComment));
     }
 
     /**
@@ -63,7 +71,16 @@ public class CommentsController {
      * @param reviewId The id of the review.
      */
     @RequestMapping(value = "/reviews/{reviewId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<Comment>> listCommentsForReview(@Valid @PathVariable("reviewId") Integer reviewId) {
-        return ResponseEntity.ok(commentRepository.findAllByReview(new Review(reviewId)));
+    public ResponseEntity<List<MongoComment>> listCommentsForReview(@Valid @PathVariable("reviewId") Integer reviewId) {
+        List<MongoComment> mongoComments = new ArrayList<>();
+        Review commentedReview = new Review(reviewId);
+        for (Comment comment : commentRepository.findAllByReview(commentedReview)) {
+            Optional<MongoComment> oCommentMongo = commentMongoRepo.findById(comment.getId());
+            if (oCommentMongo.isPresent()) {
+                mongoComments.add(oCommentMongo.get());
+            }
+            
+        }
+        return ResponseEntity.ok(mongoComments);
     }
 }
